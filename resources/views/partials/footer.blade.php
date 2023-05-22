7a61cea43dfcd307011f87a1314a9e85fe8d7cd5
raffaelecapaldo <raffaelecpl@gmail.com>
2023-05-22 17:21:23 +0200

    Added footerBottom

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -34,4 +34,22 @@
             </div>
         </div>
     </div>
+    <div class="footer-bottom">
+        <div class="container cont-fbottom d-flex flex-column gap-5 gap-md-0 flex-md-row justify-content-between align-items-center">
+            <div class="call-to-action">
+                <a class="text-uppercase" href="#">sign-up now!</a>
+            </div>
+            <div class="social d-flex align-items-center flex-column flex-sm-row">
+                <span>Follow Us</span>
+                <div class="social-logo d-flex align-items-center">
+                <img src="/img/footer-facebook.png" alt="facebook">
+                <img src="/img/footer-twitter.png" alt="twitter">
+                <img src="/img/footer-youtube.png" alt="youtube">
+                <img src="/img/footer-pinterest.png" alt="pinterest">
+                <img src="/img/footer-periscope.png" alt="periscope">
+            </div>
+
+            </div>
+        </div>
+     </div>
 </footer>
